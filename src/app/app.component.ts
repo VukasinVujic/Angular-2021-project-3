8c56a74724c46e43976cd51dad47db9902af2ea4
vukasinvujic <vukasinvujic021@gmail.com>
2021-09-07 18:24:04 +0200

    Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'test server', content: 'just a test'}];
+  serverElements = [
+    { type: 'server', name: 'test server', content: 'just a test' },
+  ];
 
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(bluprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluprintData.serverName,
+      content: bluprintData.serverContent,
+    });
+  }
 }
